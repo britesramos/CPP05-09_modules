7a1bd1db8a181fe04d89e635c325e0c4e4056f25
Sara Ramos <sara@Saras-MacBook-Air.local>
2025-07-13 11:19:13 -0700

    Ex03

--- ex03/AForm.hpp
+++ ex03/AForm.hpp
@@ -21,7 +21,7 @@ class AForm
 		AForm(std::string name, int gradeToSign, int gradeToExecute, std::string target);
 		AForm(const AForm &other);
 		AForm &operator=(const AForm &other);
-		~AForm();
+		virtual ~AForm();
 	
 		class GradeTooHighException: public std::exception{
 			public:
--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -8,5 +8,13 @@ class Intern
 		Intern();
 		~Intern();
 
+		class FormDoesNotExist: public std::exception{
+			public:
+				const char* what() const noexcept override;
+		};
+
+		static AForm* createPresidential(std::string &target);
+		static AForm* createRobotomy(std::string &target);
+		static AForm* createShrubbery(std::string &target);
 		AForm* makeForm(std::string formName, std::string target);
 };
