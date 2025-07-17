de071091eb05cb219eb9d6162c0db916c72a7b41
sramos <sramos@student.codam.nl>
2025-07-17 17:36:03 +0200

    cpp06 - ex00 - start to understand

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -1,8 +1,26 @@
 #include "ScalarConverter.hpp"
+#include <iomanip>
 
 void ScalarConverter::convert(std::string str){
 	//If a conversion to char is not displayable, print an informative message.
-	if (std::isprint(static_cast<unsigned char>(str[0]))){
-		std::cout << "YEAH!" << std::endl;
+	if (str.length() == 1 && !std::isdigit(str[0])){
+		char c = str[0];
+		std::cout << "char: " << c << std::endl;
+		std::cout << "int: " << static_cast<int>(c) << std::endl;\
+		std::cout << "float: " << std::fixed << std::setprecision(1) << static_cast<float>(c) << "f" << std::endl; //TODO: Learn about this.
+		std::cout << "double: " << static_cast<double>(c) << std::endl;
+		return ;
 	}
+
+	else if (str.length() == 1 && std::isdigit(str[0])){
+		int i = static_cast<int>(str[0]) - 48;
+		std::cout << "char: Non displayable" << std::endl;
+		std::cout << "int: " << i << std::endl;
+		std::cout << "float: " << static_cast<float>(i) << std::endl;
+		std::cout << "double: " << static_cast<double>(i) << std::endl;
+		return ;
+	}
+
+	else
+		std::cout << "Non displayable character. Please provide a string literal." << std::endl;
 }
--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -1,15 +1,16 @@
 #include "ScalarConverter.hpp"
 
 int main(int argc, char *argv[]){
-    std::string str = argv[1];
-    std::cout << "ARGV[1]: " << argv[1] << std::endl;
-    std::cout << str.size() << std::endl;
-    //NOTE: size() will not work because 42.0f is a valid input of a float.
-    if (argc == 2 && str.size() == 1){
-        ScalarConverter::convert(str);
-    }
-    else{
-        std::cout << "Wrong input. Please provide a displayable character." << std::endl;
+	if (argc == 2){
+		std::string str = argv[1];
+		// std::cout << "ARGV[1]: " << argv[1] << std::endl;
+		// std::cout << str.size() << std::endl;
+		//NOTE: size() will not work because 42.0f is a valid input of a float.
+		ScalarConverter::convert(str);
+		
+	}
+	else{
+        std::cout << "Wrong input. Please provide a displayable character. (String literals only.)" << std::endl;
         return 1;
     }
     return 0;
